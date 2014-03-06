7a8170d73bd3c65362567f82ceab5d49f266d477
Kelly Smith <ksmith@innobdesign.com>
2014-03-05 20:00:24 -0500

    Adding in HTML skeleton and initial JS code.

---
+++ staticeditor/js/main.js
@@ -0,0 +1,37 @@
+/*****************
+ *	CPSC 437 Final Project
+ *
+*/
+
+
+// Create Editor Namespace
+(function(){
+	var editor = {};
+
+	function Editor(container, content, options){
+		var self = this;
+
+		// Steps
+
+	}
+
+	editor.initEditor = function(container, content, options){
+		if( window.jQuery ){
+			return new Editor(container, content, options);
+		}
+		else{
+			console.error("Must load jQuery before loading the editor.");
+			return;
+		}
+	}
+
+})();
+
+
+
+$(document).ready(function(){
+	var dummyContent = "<h1>testing</h1>";
+
+	// Load up the editor
+	var editor = initEditor("#editor", dummyContent, {});
+});
