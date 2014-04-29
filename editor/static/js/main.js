af82195ab5e3699fbbd54b50457ee44c9b076cc2
Renee Waddell <rcw1236@gmail.com>
2014-04-28 18:03:06 -0700

    Dialog Box Radio Buttons Create File Button Stuff

--- editor/static/js/main.js
+++ editor/static/js/main.js
@@ -145,28 +145,31 @@ var sruide = {};
 			// https://jqueryui.com/dialog/#modal-form
 			$("#create-file-dialog").dialog({
 			      autoOpen: true,
-			      height: 300,
+			      height: 400,
 			      width: 350,
 			      modal: true,
 			      buttons: {
 			        "Create File": function() {
+			          //alert(document.getElementById("name").value);
 			          var bValid = true;
 			          allFields.removeClass( "ui-state-error" );
 			 
 			          bValid = bValid && checkLength( name, "file name", 3, 200 );
 			 
-			          //bValid = bValid && checkRegexp( name, /^[a-z]([0-9a-z_])+$/i, "Username may consist of a-z, 0-9, underscores, begin with a letter." );
-			          // From jquery.validate.js (by joern), contributed by Scott Gonzalez: http://projects.scottsplayground.com/email_address_validation/
-			          //bValid = bValid && checkRegexp( email, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i, "eg. ui@jquery.com" );
-			          //bValid = bValid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
-			 
 			          if ( bValid ) {
+			        	  
 			            // CREATE THE NEW FILE...
-			        	  var file = {};
+			        	  var file = {
+			        			  		                     
+			        			file_path:name.val(),
+			        			file_type:$( "input:radio[name=radio]:checked" ).val(),
+			        			filename:name.val(),
+			        			content:""
+			        	  };
 			        	  // add the right file information from the path...
 			        	  // You might need to put in empty/fake data to get it to create the file.
 			        	  /*
-			        	   *  filename = models.CharField(max_length=50) // required
+			        	   *    filename = models.CharField(max_length=50) // required
 							    file_type = models.CharField(max_length=4, choices=FILE_CHOICES, default=HTML) // they need to be able to choose this
 							    file_path = models.CharField(max_length=255) // from the file path they enter		    
 					
